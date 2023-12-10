79fde0b5e5e6b8aaba06ce7e101e178ef128756b
mutoe <imutoe@gmail.com>
2023-12-11 00:45:40 +0800

    test: ignore linebreak change in this project

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig(
     files: ['{src,test}/**/*.ts'],
     rules: {
       'import/order': 'off',
+      'style/linebreak-style': 'off',
       'unicorn/catch-error-name': 'off',
       'unicorn/no-array-for-each': 'off',
       'unicorn/no-array-reduce': 'off',
