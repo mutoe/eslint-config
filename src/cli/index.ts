2084d125f50b361b169934609128cce9f8a0a521
mutoe <imutoe@gmail.com>
2024-08-28 12:53:53 +0800

    chore: rename exported name to defineConfig
    (cherry picked from commit c9af64c30c5c9aa1c98ad07ef9e609cf3d34e27f)
    
    (cherry picked from commit 35d209caddf4d11f1ec3692b25726f6e73c44b70)

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -10,11 +10,11 @@ import { run } from './run'
 
 function header(): void {
   console.log('\n')
-  p.intro(`${c.green(`@antfu/eslint-config `)}${c.dim(`v${pkgJson.version}`)}`)
+  p.intro(`${c.green(`@mutoe/eslint-config `)}${c.dim(`v${pkgJson.version}`)}`)
 }
 
 const instance = yargs(hideBin(process.argv))
-  .scriptName('@antfu/eslint-config')
+  .scriptName('@mutoe/eslint-config')
   .usage('')
   .command(
     '*',
